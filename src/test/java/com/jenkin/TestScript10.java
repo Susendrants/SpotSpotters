32d653223faa33bfe8ff856e808c0e0bbdae66d0
SHRU <SHRU@DESKTOP-56HO2UA>
2023-11-28 13:32:31 +0530

    aa

---
+++ src/test/java/com/jenkin/TestScript10.java
@@ -0,0 +1,13 @@
+package com.jenkin;
+
+import org.testng.annotations.Test;
+
+public class TestScript10 {
+	
+	@Test
+	public void practice1()
+	{
+		System.out.println("pracrice is create");
+	}
+
+}
