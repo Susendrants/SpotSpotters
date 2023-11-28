4bce9c435444fba8cf92f81d4b887a1bdef7a9d9
susendran <susee.jt@gmail.com>
2023-11-28 13:41:51 +0530

    kbkk

--- src/test/java/com/jenkin/TestScript4.java
+++ src/test/java/com/jenkin/TestScript4.java
@@ -2,19 +2,13 @@ package com.jenkin;
 
 import org.testng.annotations.Test;
 
-public class TestScript3 {
-<<<<<<< HEAD
+public class TestScript4 {
+
 	@Test
 	public void verify()
 	{
 		System.out.println("shruthi verify");
-=======
-	
-	@Test
-	public void create()
-	{
-		System.out.println("test is created");
->>>>>>> branch 'master' of https://github.com/Susendrants/SpotSpotters.git
 	}
-
+	
+	
 }
---
+++ src/test/java/com/jenkin/testScript5.java
@@ -0,0 +1,12 @@
+package com.jenkin;
+
+import org.testng.annotations.Test;
+
+public class testScript5 {
+	@Test
+	public void ts5() {
+		System.out.println("hi");
+	}
+	
+
+}
