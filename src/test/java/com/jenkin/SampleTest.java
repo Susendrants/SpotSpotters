b416fa1918f677d3cd20311f5549f0726afee38a
susendrants <susee.jt@gmail.com>
2023-11-30 12:53:07 +0530

    chrome

---
+++ src/test/java/com/jenkin/SampleTest.java
@@ -0,0 +1,15 @@
+package com.jenkin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	WebDriver driver;
+@Test
+public void m1Test()
+{
+	driver=new ChromeDriver();
+	driver.get("https://www.facebook.com");
+}
+}
